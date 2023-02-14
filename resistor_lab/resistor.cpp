bcbac07e503941b69fff0177f1490e39fcf95104
apkevin <86992003+apkevin@users.noreply.github.com>
2023-02-13 23:47:32 -0600

    Add files via upload

---
+++ resistor_lab/resistor.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+// Initializing variables.
+double resistance_1;
+double resistance_2;
+
+cout << "Welcome to the Resistor Calculation Program!\n\n";
+
+// Receiving input from user and storing input into variables.
+cout << "Please enter a resistance: ";
+cin >> resistance_1;
+cout << "Please enter a 2nd resistance: ";
+cin >> resistance_2;
+cout << "\nThank You! You've entered " << resistance_1 << "ohms and " << resistance_2 << "ohms.\n\n"
+<< "Calculating ... \n\n" << "Done\n\n";
+
+// assigning series and parallel calculations. 
+double series = resistance_1 + resistance_2;
+double parallel = 1 / (1 / resistance_1 + 1/ resistance_2);
+
+// Displahying results back to user.
+cout << "If your resistors are placed in series, they'll total to " << series << " ohms.\n\n";
+cout << "If they are placed in parallel, they'll total " << parallel << " ohms.\n\n";
+cout << "Thank You for using the RCP!!\n\n" << "Endeavor to have a blossoming day\n";
+}
