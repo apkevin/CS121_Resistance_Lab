eedf1e0b0aa2c8a6fbf160137ea04d5c2286ae43
apkevin <86992003+apkevin@users.noreply.github.com>
2023-03-09 21:23:58 -0600

    Add files via upload

--- resistor_lab/resistor.cpp
+++ resistor_lab/resistor.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <limits>
 using namespace std;
+char input;
 
 int main() {
 // Initializing variables.
@@ -9,16 +11,39 @@ double resistance_2;
 cout << "Welcome to the Resistor Calculation Program!\n\n";
 
 // Receiving input from user and storing input into variables.
+
 cout << "Please enter a resistance: ";
 cin >> resistance_1;
+while (cin.fail()) {
+
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cerr << "\nPlease enter just numbers!\n\n";
+    
+    cin >> resistance_1;
+
+}
+
+
 cout << "Please enter a 2nd resistance: ";
 cin >> resistance_2;
+while (cin.fail()) {
+
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cerr << "\nPlease enter just numbers!\n\n";
+    
+    cin >> resistance_2;
+}
+
+
+
 cout << "\nThank You! You've entered " << resistance_1 << "ohms and " << resistance_2 << "ohms.\n\n"
 << "Calculating ... \n\n" << "Done\n\n";
 
 // assigning series and parallel calculations. 
 double series = resistance_1 + resistance_2;
-double parallel = 1 / (1 / resistance_1 + 1/ resistance_2);
+double parallel = 1 / (1 / resistance_1 + 1 / resistance_2);
 
 // Displahying results back to user.
 cout << "If your resistors are placed in series, they'll total to " << series << " ohms.\n\n";
